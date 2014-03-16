bb4149c7c43a75905794290aad4f239fd050020d
DENIELER <danielostapenko@gmail.com>
2014-03-16 22:33:30 +0200

    Add VkSearch project

---
+++ VKSearch/Controllers/HomeController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VKSearch.Models;
+
+namespace VKSearch.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        string connectionString = @"Data Source=DANIEL\SQLEXPRESS;Initial Catalog=VkSearch;Integrated Security=SSPI;";
+
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewResults()
+        {
+            var result = new List<VkUser>();
+
+            using (SqlConnection sqlcon = new SqlConnection(connectionString))
+            {
+                sqlcon.Open();
+
+                using (SqlCommand sqlcom = new SqlCommand())
+                {
+                    sqlcom.Connection = sqlcon;
+                    sqlcom.CommandText = @"SELECT * FROM searchResult (nolock) where checked = 0";
+
+                    var reader = sqlcom.ExecuteReader();
+                    if(reader!=null)
+                    {
+                        while (reader.Read())
+                        {
+                            result.Add(new VkUser()
+                            {
+                                first_name = (string)reader["firstName"],
+                                last_name = (string)reader["lastName"],
+                                id = Convert.ToInt64((string)reader["userId"]),
+                                photo_200_orig = (string)reader["photo"],
+                                status = (string)reader["info"]
+                            });
+                        }
+                    }
+                }
+            }
+            return View("ViewResults", result);
+        }
+
+        public JsonResult CheckSearchResult(long userId)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(connectionString))
+            {
+                sqlcon.Open();
+
+                using (SqlCommand sqlcom = new SqlCommand())
+                {
+                    sqlcom.Connection = sqlcon;
+                    sqlcom.CommandText = @"UPDATE searchResult
+                                            SET checked = 1
+                                            WHERE userId = @userId";
+
+                    sqlcom.Parameters.AddWithValue("@userId", userId);
+                    sqlcom.ExecuteNonQuery();
+                }
+            }
+
+            return Json(true);
+        }
+
+        [HttpPost]
+        public JsonResult AddSearchResult(VkUser user)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(connectionString))
+            {
+                sqlcon.Open();
+
+                bool isExists = false;
+                using (SqlCommand sqlcom = new SqlCommand())
+                {
+                    sqlcom.Connection = sqlcon;
+                    sqlcom.CommandText = @"SELECT 1 FROM searchResult
+                                            WHERE userId = @userId";
+
+                    sqlcom.Parameters.AddWithValue("@userId", user.id);
+
+                    object result = sqlcom.ExecuteScalar();
+                    if (result != null)
+                        isExists = true;
+                }
+
+                if (!isExists)
+                {
+                    using (SqlCommand sqlcom = new SqlCommand())
+                    {
+                        sqlcom.Connection = sqlcon;
+                        sqlcom.CommandText = @"INSERT INTO searchResult
+                                            VALUES (@userId, @firstName, @lastName, @photo, @info, @checked)";
+
+                        sqlcom.Parameters.AddWithValue("@userId", user.id);
+                        sqlcom.Parameters.AddWithValue("@firstName", user.first_name);
+                        sqlcom.Parameters.AddWithValue("@lastName", user.last_name);
+                        sqlcom.Parameters.AddWithValue("@photo", user.photo_200_orig);
+                        sqlcom.Parameters.AddWithValue("@info", "Birthday:" + user.bdate + ". Status: " + user.status);
+                        sqlcom.Parameters.AddWithValue("@checked", false);
+
+                        sqlcom.ExecuteNonQuery();
+                    }
+                }
+            }
+
+            return Json(true);
+        }
+    }
+}
---
+++ VKSearch/Models/VkUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VKSearch.Models
+{
+    public class VkUser
+    {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string screen_name { get; set; }
+        public string bdate { get; set; }
+        public int hasMobile { get; set; }
+
+        public long id { get; set; }
+        public string photo_100 { get; set; }
+        public string photo_200_orig { get; set; }
+
+        public string status { get; set; }
+    }
+}
